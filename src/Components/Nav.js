1624dc4caa220dec431465c29f3b0a08aa58f44d
Gregory Bertrand <gregoryalexbertrand@gmail.com>
2021-06-13 11:51:34 -0500

    finished navbar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,13 +3,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faUser} from '@fortawesome/free-solid-svg-icons'
 import {faSearch} from '@fortawesome/free-solid-svg-icons'
 import {faShoppingBag} from '@fortawesome/free-solid-svg-icons'
+import {faBars} from '@fortawesome/free-solid-svg-icons'
 
 const Nav = () => {
     return (
         <div className="nav-bar">
-            {/* <h4 className="logo">Arizona</h4> */}
-            <ul className="links">
+            <div className="hamburger-icon">
+                <FontAwesomeIcon icon={faBars} />
+            </div>
+            <div className="arizona">
             <h4 className="logo">Arizona</h4>
+            </div>
+            <div className="nav-links">
+            <ul className="links">
                 <li>Drinks</li>
                 <li>Sun Brew</li>
                 <li>Mixes</li>
@@ -20,9 +26,12 @@ const Nav = () => {
                 <li>International</li>
                 <li>About Us</li>
             </ul>
-                <FontAwesomeIcon icon={faUser} />
-                <FontAwesomeIcon icon={faSearch} />
-                <FontAwesomeIcon icon={faShoppingBag} />
+            </div>
+            <div className="checkout-icons">
+            <FontAwesomeIcon icon={faUser} className="icon"/>
+                <FontAwesomeIcon icon={faSearch} className="icon"/>
+                <FontAwesomeIcon icon={faShoppingBag} className="icon"/>
+            </div>
         </div>
     )
 }
