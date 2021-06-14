8262fdca2adbad3ec0f4a180b5b722dd0370fcb4
Gregory Bertrand <gregoryalexbertrand@gmail.com>
2021-06-14 14:12:41 -0500

    first card done

--- src/Pages/ProductPage.js
+++ src/Pages/ProductPage.js
@@ -7,7 +7,12 @@ const ProductPage = () => {
     return (
         <div className="product-page">
             <Card className="product-card">
-                <p>product</p>
+                <img src="https://cdn.shopify.com/s/files/1/0087/6065/5938/products/az-shopify-product-KRAFT_10CT_AP_883d2677-5c30-46a3-b406-20e7db509de9_2000x.png?v=1616427536" />
+                <h4>Arnold Palmer Half & Half</h4>
+                <h5>Powder Stix</h5>
+                <span className="price">$29.99</span>
+                <span className="quantity">(case of 12)</span>
+                <button className="addToCart">Add to Cart</button>
             </Card>
             <Card className="product-card">
                 <p>product</p>
